2661b5229306aa3199c28914efd7eaddb040a202
usha-12 <usha12ahirwar@gmail.com>
2022-11-01 21:57:55 +0530

    Print Normal Array

--- src/usha/generic/ArraysAccess.java
+++ src/usha/generic/ArraysAccess.java
@@ -1,2 +1,30 @@
-package usha.generic;public class ArraysAccess {
+package usha.generic;
+
+public class ArraysAccess {
+    public static void printArrayInt(int[] array){
+        for (int x :array){
+            System.out.println(x);
+        }
+    }
+    public static void printArrayFloat(float[] array){
+        for (float x :array){
+            System.out.println(x);
+        }
+    }
+    public static void printArrayString(String[] array){
+        for (String x :array){
+            System.out.println(x);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arrInt = {23,34,56,7,88,4};
+        float[] arrFloat = {1.3f,12.6f, 4.5f, 45.4f, 10.1f, 56.89f };
+        String[] arrString = {"usha", "Sankey", "tsar", "aditya"};
+        printArrayInt(arrInt);
+        System.out.println("------------------------------------------------------------------------");
+        printArrayFloat(arrFloat);
+        System.out.println("-------------------------------------------------------------------------");
+        printArrayString(arrString);
+    }
 }
